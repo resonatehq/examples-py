330040bc0c6cc44d9bff7aef01575b6f1bfdd139
Tomperez98 <tomasperezalvarez@gmail.com>
2024-07-17 17:48:04 -0500

    Sequential example and optimistic locking

---
+++ race-condition/tests/conftest.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pytest
+
+
+@pytest.fixture()
+def setup_and_teardown() -> sqlite3.Connection:
+    conn = sqlite3.connect("test.db")
+    ans = conn.execute("SELECT 1").fetchone()
+    assert ans == (1,)
+    conn.execute("DROP TABLE IF EXISTS accounts")
+    return conn
