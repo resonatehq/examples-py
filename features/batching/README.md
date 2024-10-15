5a6236d4e429ec992e9bdc30896ed7472f9a917e
Tomperez98 <tomasperezalvarez@gmail.com>
2024-10-15 16:55:58 -0500

    Add batching example

---
+++ features/batching/README.md
@@ -0,0 +1,78 @@
+# Batching
+## Still sequential looking code, simple, but faster and cheaper.
+Resonate transparent batching enables you to take the most out of bulk operation againts other services. Saving money and time (by making a single http call) while running code that looks sequential. 
+
+```py
+# With batching
+
+conn = Connection(":memory:", check_same_thread=False)
+
+resonate = Scheduler(LocalPromiseStore(), processor_threads=1)
+
+@dataclass
+class InsertValue(Command):
+    conn: Connection
+    value: int
+
+
+def _batch_handler(cmds: list[InsertValue]):
+    first_value = cmds[0].value
+    last_value = None
+    for cmd in cmds:
+        conn.execute("INSERT INTO benchmark (value) VALUES (?)", (cmd.value,))
+        last_value = cmd.value
+    conn.commit()
+    print(f"Values from {first_value} to {last_value} have been inserted to database.")
+
+
+def create_user_batching(ctx: Context, v: int):
+    p = yield ctx.lfi(InsertValue(conn, v))
+    yield p
+
+# register top level function.
+resonate.register(create_user_batching, retry_policy=never())
+
+# register batch handler
+resonate.register_command_handler(InsertValue, _batch_handler, retry_policy=never())
+
+```
+
+```bash
+rye run batching --batch --values 1_000
+# Inserting 1,000 values took 0.092471 seconds with batching=True
+rye run batching --batch --values 10_000
+# Inserting 10,000 values took 0.864541 seconds with batching=True
+rye run batching --batch --values 100_000
+# Inserting 100,000 values took 12.509531 seconds with batching=True
+```
+
+```py
+# Without batching
+
+conn = Connection(":memory:", check_same_thread=False)
+
+resonate = Scheduler(LocalPromiseStore(), processor_threads=1)
+
+def _create_user(ctx: Context, value: int):
+    conn.execute("INSERT INTO benchmark (value) VALUES (?)", (value,))
+    conn.commit()
+    print(f"Value {value} has been inserted to database")
+
+
+def create_user_sequentially(ctx: Context, v: int):
+    p = yield ctx.lfi(_create_user, v).with_options(retry_policy=never())
+    yield p
+
+# register top level function.
+resonate.register(create_user_sequentially, retry_policy=never())
+
+```
+
+```bash
+rye run batching --batch --values 1_000
+# Inserting 1,000 values took 0.138040 seconds with batching=False
+rye run batching --batch --values 10_000
+# Inserting 10,000 values took 1.725269 seconds with batching=False
+rye run batching --batch --values 100_000
+# Inserting 100,000 values took 17.681185 seconds with batching=False
+```
---
+++ features/batching/src/batching/__init__.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+from resonate.context import Context
+from sqlite3 import Connection
+from resonate.scheduler import Scheduler
+from resonate.storage import LocalPromiseStore
+import click
+from resonate.retry_policy import never
+from resonate.commands import Command
+
+import time
+
+
+conn = Connection(":memory:", check_same_thread=False)
+resonate = Scheduler(LocalPromiseStore(), processor_threads=1)
+
+
+def _create_user(ctx: Context, value: int):
+    conn.execute("INSERT INTO benchmark (value) VALUES (?)", (value,))
+    conn.commit()
+    print(f"Value {value} has been inserted to database")
+
+
+def create_user_sequentially(ctx: Context, v: int):
+    p = yield ctx.lfi(_create_user, v).with_options(retry_policy=never())
+    yield p
+
+
+@dataclass
+class InsertValue(Command):
+    conn: Connection
+    value: int
+
+
+def _batch_handler(cmds: list[InsertValue]):
+    first_value = cmds[0].value
+    last_value = None
+    for cmd in cmds:
+        conn.execute("INSERT INTO benchmark (value) VALUES (?)", (cmd.value,))
+        last_value = cmd.value
+    conn.commit()
+    print(f"Values from {first_value} to {last_value} have been inserted to database.")
+
+
+def create_user_batching(ctx: Context, v: int):
+    p = yield ctx.lfi(InsertValue(conn, v))
+    yield p
+
+
+# register top level function.
+resonate.register(create_user_sequentially, retry_policy=never())
+resonate.register(create_user_batching, retry_policy=never())
+
+# register batch handler
+resonate.register_command_handler(InsertValue, _batch_handler, retry_policy=never())
+
+
+@click.command()
+@click.option("--batch/--no-batch", default=False)
+@click.option("--values", type=click.IntRange(0, 100_000))
+def cli(batch: bool, values: int):
+    # conn.execute("DROP TABLE IF EXISTS benchmark")
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS benchmark (id INTEGER PRIMARY KEY, value INTEGER)"
+    )
+    conn.commit()
+    promises = []
+
+    start_time = time.time_ns()
+    if batch:
+        for v in range(values):
+            p = resonate.run(f"insert-value-{v}", create_user_batching, v)
+            promises.append(p)
+    else:
+        for v in range(values):
+            p = resonate.run(f"insert-value-{v}", create_user_sequentially, v)
+            promises.append(p)
+
+    for p in promises:
+        p.result()
+
+    end_time = time.time_ns()
+    print(
+        f"Inserting {values:,} values took {(end_time-start_time)/1e9:2f} seconds with batching={batch}"
+    )
+
+
+def main() -> None:
+    cli()
