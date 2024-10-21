c864dbe491680e66892ee3953aaa841f66722001
Cully Wakelin <cully.wakelin@resonatehq.io>
2024-10-21 14:14:14 -0600

    consistenty in examples

--- features/batching-benchmark/src/batching/__init__.py
+++ features/batching-benchmark/src/batching/__init__.py
@@ -1,27 +1,27 @@
+# @@@SNIPSTART examples-py-features-batching-benchmark-init
 from dataclasses import dataclass
 from resonate.context import Context
 from resonate.scheduler import Scheduler
 from resonate.storage import LocalPromiseStore
 from resonate.retry_policy import never
 from resonate.commands import Command
-from sqlite3 import Connection
 import sqlite3
 import click
 import time
 
 # Create an SQLite database if it doesn't exist
 # Create a connection with that database
-conn = sqlite3.connect("benchmark.db", check_same_thread=False)
+conn = sqlite3.connect("your_database.db", check_same_thread=False)
 
 # Create a Resonate Scheduler with an in memore promise store
 resonate = Scheduler(LocalPromiseStore(), processor_threads=1)
 
 ### SEQUENTIAL INSERTS
 # Define a function that inserts a single row into the database
-def _create_user(_: Context, value: int):
+def _create_user(ctx: Context, value: int):
     conn.execute("INSERT INTO users (value) VALUES (?)", (value,))
     conn.commit()
-    print(f"Value {value} has been inserted to database")
+    print(f"User {value} has been inserted to database")
 
 # Define a top level function that uses sequential inserts
 def create_user_sequentially(ctx: Context, v: int):
@@ -31,24 +31,23 @@ def create_user_sequentially(ctx: Context, v: int):
 ### BATCH INSERTS
 # Define a data structure for the Resonate SDK to track and create batches of
 @dataclass
-class InsertValue(Command):
-    conn: Connection
-    value: int
+class InsertUser(Command):
+    id: int
 
 # Define a function that inserts a batch of rows into the database
 # The main difference is that commit() is only called after all the Insert statements are executed
-def _batch_handler(_: Context, cmds: list[InsertValue]):
-    first_value = cmds[0].value
-    last_value = None
-    for cmd in cmds:
-        conn.execute("INSERT INTO users (value) VALUES (?)", (cmd.value,))
-        last_value = cmd.value
+def _batch_handler(ctx: Context, users: list[InsertUser]):
+    first_user = users[0].id
+    last_user = None
+    for user in users:
+        conn.execute("INSERT INTO users (value) VALUES (?)", (user.id,))
+        last_user = user.id
     conn.commit()
-    print(f"Values from {first_value} to {last_value} have been inserted to database.")
+    print(f"Users from {first_user} to {last_user} have been inserted to database.")
 
 # Definte the top level function that uses batching
-def create_user_batching(ctx: Context, v: int):
-    p = yield ctx.lfi(InsertValue(conn, v))
+def create_user_batching(ctx: Context, u: int):
+    p = yield ctx.lfi(InsertUser(u))
     yield p
 
 # Register the top level functions with the Resonate Scheduler
@@ -56,13 +55,13 @@ resonate.register(create_user_sequentially, retry_policy=never())
 resonate.register(create_user_batching, retry_policy=never())
 
 # Register the batch handler and data structure with the Resonate Scheduler
-resonate.register_command_handler(InsertValue, _batch_handler, retry_policy=never())
+resonate.register_command_handler(InsertUser, _batch_handler, retry_policy=never())
 
 # Define a CLI to create an interaction point
 @click.command()
 @click.option("--batch/--no-batch", default=False)
-@click.option("--values", type=click.IntRange(0, 100_000))
-def cli(batch: bool, values: int):
+@click.option("--users", type=click.IntRange(0, 100_000))
+def cli(batch: bool, users: int):
     # To benchmark, we start from a clean slate each time
     # Drop the users table if it already exists
     conn.execute("DROP TABLE IF EXISTS users")
@@ -77,13 +76,13 @@ def cli(batch: bool, values: int):
     start_time = time.time_ns()
     # If batching, run the batch inserts
     if batch:
-        for v in range(values):
-            p = resonate.run(f"insert-batch-value-{v}", create_user_batching, v)
+        for v in range(users):
+            p = resonate.run(f"insert-batch-user-{v}", create_user_batching, v)
             promises.append(p)
     # If not batching, run the sequential inserts
     else:
-        for v in range(values):
-            p = resonate.run(f"insert-no-batch-value-{v}", create_user_sequentially, v)
+        for v in range(users):
+            p = resonate.run(f"insert-no-batch-user-{v}", create_user_sequentially, v)
             promises.append(p)
 
     # Yield all promises to ensure they are all complete
@@ -93,8 +92,9 @@ def cli(batch: bool, values: int):
     # Capture the ending time of the operation
     end_time = time.time_ns()
     print(
-        f"Inserting {values:,} values took {(end_time-start_time)/1e9:2f} seconds with batching={batch}"
+        f"Inserting {users:,} users took {(end_time-start_time)/1e9:2f} seconds with batching={batch}"
     )
 
 def main() -> None:
     cli()
+# @@@SNIPEND
--- features/batching/src/batching/__init__.py
+++ features/batching/src/batching/__init__.py
@@ -1,10 +1,10 @@
+# @@@SNIPSTART examples-py-features-batching-init
 from dataclasses import dataclass
 from resonate.context import Context
 from resonate.scheduler import Scheduler
 from resonate.storage import LocalPromiseStore
 from resonate.retry_policy import never
 from resonate.commands import Command
-from sqlite3 import Connection
 import sqlite3
 
 # Create an SQLite database if it doesn't exist
@@ -16,31 +16,30 @@ resonate = Scheduler(LocalPromiseStore(), processor_threads=1)
 
 # Define a data structure for the Resonate SDK to track and create batches of
 @dataclass
-class InsertValue(Command):
-    conn: Connection
-    value: int
+class InsertUser(Command):
+    id: int
 
 # Define a function that inserts a batch of rows into the database
 # The main difference is that commit() is only called after all the Insert statements are executed
-def _batch_handler(_: Context, cmds: list[InsertValue]):
-    first_value = cmds[0].value
+def _batch_handler(_: Context, users: list[InsertUser]):
+    first_value = users[0].id
     last_value = None
-    for cmd in cmds:
-        conn.execute("INSERT INTO users (value) VALUES (?)", (cmd.value,))
-        last_value = cmd.value
+    for user in users:
+        conn.execute("INSERT INTO users (value) VALUES (?)", (user.id,))
+        last_value = user.id
     conn.commit()
     print(f"Values from {first_value} to {last_value} have been inserted to database.")
 
 # Definte the top level function that uses batching
-def create_user_batching(ctx: Context, v: int):
-    p = yield ctx.lfi(InsertValue(conn, v))
+def create_user_batching(ctx: Context, u: int):
+    p = yield ctx.lfi(InsertUser(u))
     yield p
 
 # Register the top level functions with the Resonate Scheduler
 resonate.register(create_user_batching, retry_policy=never())
 
 # Register the batch handler and data structure with the Resonate Scheduler
-resonate.register_command_handler(InsertValue, _batch_handler, retry_policy=never())
+resonate.register_command_handler(InsertUser, _batch_handler, retry_policy=never())
 
 def main() -> None:
     # Drop the users table if it already exists
@@ -52,9 +51,10 @@ def main() -> None:
     # Create an array to hold the promises
     promises = []
 
-    for v in range(10000):
-        p = resonate.run(f"insert-value-{v}", create_user_batching, v)
+    for u in range(10000):
+        p = resonate.run(f"insert-value-{u}", create_user_batching, u)
         promises.append(p)
 
     for p in promises:
         p.result()
+# @@@SNIPEND
